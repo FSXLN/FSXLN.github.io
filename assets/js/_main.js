a39a63e8ed03af1d56c106349ca37efa7c41f605
FSXLN <1548485735@qq.com>
2026-06-29 11:23:23 +0000

    v1.2

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -104,8 +104,9 @@ $(document).ready(function () {
 
   // Enable the sticky footer
   var bumpIt = function () {
+    var footerHeight = $(".page__footer").outerHeight(true) || 0;
     $("body").css("padding-bottom", "0");
-    $("body").css("margin-bottom", $(".page__footer").outerHeight(true));
+    $("body").css("margin-bottom", footerHeight);
   }
   $(window).resize(function () {
     didResize = true;
